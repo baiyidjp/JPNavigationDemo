8d58e88a4d52daa9755766246a6d114e781a3d51
jp <jiangpeng@cecelive.com>
2018-07-10 10:04:55 +0800

    Initial Commit

---
+++ JPNavigationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JPNavigationDemo
+//
+//  Created by baiyi on 2018/7/10.
+//  Copyright © 2018年 dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JPNavigationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JPNavigationDemo
+//
+//  Created by baiyi on 2018/7/10.
+//  Copyright © 2018年 dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
